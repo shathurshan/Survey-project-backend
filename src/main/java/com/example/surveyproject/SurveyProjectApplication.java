c3505f511c5d7378f70e9f32a4c1654774aa2b55
shathurshan <ravishathu856@gmail.com>
2023-04-25 15:27:14 +0530

    firsh commit initiate the project

---
+++ src/main/java/com/example/surveyproject/SurveyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/surveyproject/models/ERole.java
@@ -0,0 +1,6 @@
+package com.example.surveyproject.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
