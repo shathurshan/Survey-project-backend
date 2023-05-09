66d8a5300eb40bac1d8d18cd19ee4cc59cc55703
shathurshan <ravishathu856@gmail.com>
2023-05-09 16:47:00 +0530

    remove the unused service files

--- src/main/java/com/example/surveyproject/security/services/SurveyPostDetailsService.java
+++ src/main/java/com/example/surveyproject/security/services/SurveyPostDetailsService.java
@@ -1,16 +1,60 @@
 package com.example.surveyproject.security.services;
 
+
+import com.example.surveyproject.exception.ResourceNotFoundException;
 import com.example.surveyproject.models.Surveys;
+import com.example.surveyproject.repository.SurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface SurveyPostDetailsService {
-    Surveys updateSurveys(Surveys surveys);
 
-    List<Surveys> getAllSurveys();
+@Service
+@Transactional
+public class SurveyPostDetailsService {
 
-    Surveys getSurveysById(String surveyId);
+    @Autowired
+    SurveyRepository surveyRepository;
 
-    void deleteSurvey(String surveyId);
+    public Surveys updateSurveys(Surveys surveys) {
+        Optional <Surveys> surveysDB = this.surveyRepository.findById(surveys.getId());
+        if (surveysDB.isPresent()) {
+            Surveys surveyUpdate = surveysDB.get();
+            surveyUpdate.setId(surveys.getId());
+            surveyUpdate.setSurveyName(surveys.getSurveyName());
+            surveyUpdate.setQuestions(surveys.getQuestions());
+           surveyRepository.save(surveyUpdate);
+            return surveyUpdate;
+        } else {
+            throw new ResourceNotFoundException("Record not found with id : " + surveys.getId());
+        }
+    }
 
+    public List<Surveys> getAllSurveys() {
+        return this.surveyRepository.findAll();
+    }
+
+
+
+    public Surveys getSurveysById(String surveyId) {
+        Optional <Surveys> surveysDB = this.surveyRepository.findById(surveyId);
+        if (surveysDB.isPresent()){
+            return surveysDB.get();
+        } else{
+            throw new ResourceNotFoundException("Record not found with id : " + surveyId);
+        }
+    }
+
+    public void deleteSurvey(String surveyId) {
+        Optional <Surveys> surveysDB = this.surveyRepository.findById(surveyId);
+
+        if (surveysDB.isPresent()){
+            this.surveyRepository.delete(surveysDB.get());
+        } else {
+            throw new ResourceNotFoundException("Record not found with id : " + surveyId);
+        }
+    }
 }
